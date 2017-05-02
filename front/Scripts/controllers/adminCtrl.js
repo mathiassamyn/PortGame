191aedb9138c676edaa19891fcddf52984419674
mathiassamyn <mathiassamyn@hotmail.com>
2017-05-02 11:38:16 +0200

    fix admin power V0.1

---
+++ front/Scripts/controllers/adminCtrl.js
@@ -0,0 +1,3 @@
+﻿app.controller("adminCtrl", ["$scope", "socket", "$cookies", "$routeParams", function ($scope, socket, $cookies, $routeParams) {
+    $cookies.put("guideID", $routeParams.guide);
+}])
