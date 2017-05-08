e1c3f247dafbbea7ccc7f216fd41f996752b4eea
mathiassamyn <mathiassamyn@hotmail.com>
2017-05-08 13:39:25 +0200

    from start region owners

--- front/Scripts/controllers/gameMasterCtrl.js
+++ front/Scripts/controllers/gameMasterCtrl.js
@@ -1,29 +1,61 @@
-﻿app.controller("gameMasterCtrl", ["$scope", "socket", "$cookies", "$state", function ($scope, socket, $cookies, $state) {
+﻿app.controller("gameMasterCtrl", ["$scope", "socket", "$cookies", "$state", "$http", "$parse", function ($scope, socket, $cookies, $state, $http, $parse) {
 
-    $scope.viewEnabled = true;
+    //REGIONS
+    //This will be used when the user reloads the page. This way he will have the right region owners from the start.
+    var getOwner = function (region) {
+        $http.get("/owner/" + region).then(
+            function successCallback(response) {
+                console.log(response);
+                for (var i = 0; i < response.data.length; i++) {
+                    var model = $parse(region);
+                    model.assign($scope, {
+                        teamID: response.data[i][0].value,
+                        team: response.data[i][1].value
+                    });
+                }
+            },
+            function errorCallback(response) {
+                console.log(response);
+            });
+    }
 
-    socket.on("pause", function () {
-        $scope.$apply(function () {
-            $scope.viewEnabled = false;
+    $scope.manufacturing = $scope.logistics = "";
+    getOwner("manufacturing");
+    getOwner("logistics");
+
+    socket.on("region", function (msg) {
+        var model = $parse(msg.region);
+        model.assign($scope, {
+            teamID: msg.teamID,
+            team: msg.team
         });
-    });
+    })
 
-    socket.on("resume", function () {
-        $scope.$apply(function () {
-            $scope.viewEnabled = true;
-        });
-    });
 
-    socket.on("start", function () {
-        $scope.$apply(function () {
-            $state.go("map");
-        });
-    });
+    //Admin functions
+	$scope.viewEnabled = true;
 
-    socket.on("stop", function () {
-        $scope.$apply(function () {
-            $state.go("endresult");
-        });
-    });
+	socket.on("pause", function () {
+		$scope.$apply(function () {
+			$scope.viewEnabled = false;
+		});
+	});
 
+	socket.on("resume", function () {
+		$scope.$apply(function () {
+			$scope.viewEnabled = true;
+		});
+	});
+
+	socket.on("start", function () {
+		$scope.$apply(function () {
+			$state.go("map");
+		});
+	});
+
+	socket.on("stop", function () {
+		$scope.$apply(function () {
+			$state.go("endresult");
+		});
+	});
 }])
--- front/Scripts/controllers/mapCtrl.js
+++ front/Scripts/controllers/mapCtrl.js
@@ -1,9 +1,4 @@
-﻿app.controller("mapCtrl", ["$scope", "$cookies", "$state", "mapChanges", function ($scope, $cookies, $state, mapChanges) {
-
-    console.log(mapChanges.manu);
-    setTimeout(function () { console.log(mapChanges.manufacturing); }, 3000);
-    $scope.manufacturing = mapChanges.manufacturing;
-    $scope.logistics = mapChanges.logistics;
+﻿app.controller("mapCtrl", ["$scope", "$cookies", "$state", function ($scope, $cookies, $state) {
 
     $scope.regionView = function (region) {
         $state.go("region." + region, { region: region });
