b832822f5cfa2b0b72ae59798e3f9d36ec9ef42d
mathiassamyn <mathiassamyn@hotmail.com>
2017-05-11 13:31:24 +0200

    :sparkles: get end result

--- front/Scripts/controllers/endResultCtrl.js
+++ front/Scripts/controllers/endResultCtrl.js
@@ -1,3 +1,15 @@
-﻿app.controller("endResultCtrl", ["$scope", function ($scope) {
-
+﻿app.controller("endResultCtrl", ["$scope", "$http", function ($scope, $http) {
+    $scope.endResult = [];
+    $http.get("/endResult").then(
+        function successCallback(response) {
+            for (var i = 0; i < response.data.length; i++) {
+                $scope.endResult[i] = {
+                    team: response.data[i][0].value,
+                    coins: response.data[i][1].value
+                }
+            }
+        },
+        function errorCallback(response) {
+            console.log(response);
+        });
 }])
